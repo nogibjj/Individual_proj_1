ad5f1189cedfe8d6b14e87b728ecd9fdcc48a98e
Fan Xu <fanxu2017@gmail.com>
2024-08-30 19:31:15 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 '''main file with main functions'''
 
 
-def subtract(x, y):
+def subtract(first_num, second_num):
     '''function to subtract two numbers'''
-    return x - y
+    return first_num - second_num
