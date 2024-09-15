8dd7c0d059ef15311bcd3d9d473ff93bdbd953e4
Fan Xu <fanxu2017@gmail.com>
2024-09-15 19:24:28 -0400

    added required packages and fixed lint errors

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-file_path = "NBA_24_stats.csv"
-df = pd.read_csv(file_path)
+FILE_PATH = "NBA_24_stats.csv"
+df = pd.read_csv(FILE_PATH)
 
 # Show the first few rows
 # print(df["Player"])
 
 
 def summary():
+    """provides summary statistics"""
     summary_stats = df.describe()
     print(summary_stats)
 
@@ -20,11 +21,11 @@ def summary():
 
 
 def points_plot():
-    player_name = df["Player"].astype(str)
+    """provides visualization"""
+    # player_name = df["Player"].astype(str)
     player_rank = df["Rk"].astype(str)
     plt.bar(player_rank, df["PTS"])
     plt.show()
-    return
 
 
 points_plot()
--- test_main.py
+++ test_main.py
@@ -1,10 +1,16 @@
-''' file for testing code'''
-from main import subtract
+""" file for testing code"""
+
+from main import summary, points_plot
 
 
-def test_subtract():
-    assert subtract(3, 2) == 1
+def test_summary():
+    summary()
+
+
+def test_plot():
+    points_plot()
 
 
 if __name__ == "__main__":
-    test_subtract
+    test_summary()
+    test_plot()
