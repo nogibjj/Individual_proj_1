f4a469e9739cd0f59cdea49309ebdb93d46ccd7a
Fan Xu <fanxu2017@gmail.com>
2024-09-15 18:15:27 -0400

    added a csv and pandas script

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 """main file with main functions"""
 
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def subtract(first_num, second_num):
-    """function to subtract two numbers"""
-    return first_num - second_num
+
+file_path = "NBA_24_stats.csv"
+df = pd.read_csv(file_path)
+
+# Show the first few rows
+# print(df["Player"])
+
+
+def summary():
+    summary_stats = df.describe()
+    print(summary_stats)
+
+
+# summary()
+
+
+def points_plot():
+    player_name = df["Player"].astype(str)
+    player_rank = df["Rk"].astype(str)
+    plt.bar(player_rank, df["PTS"])
+    plt.show()
+    return
+
+
+points_plot()
